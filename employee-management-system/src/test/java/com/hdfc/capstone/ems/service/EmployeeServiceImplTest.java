bbc44a228f30d553d829e771b33d6d65d4e5463a
bharath210 <barathkumargoudpalsa@gmail.com>
2023-04-24 14:27:13 +0530

    Fixed Junit error

--- employee-management-system/src/test/java/com/hdfc/capstone/ems/service/EmployeeServiceImplTest.java
+++ employee-management-system/src/test/java/com/hdfc/capstone/ems/service/EmployeeServiceImplTest.java
@@ -1,24 +1,24 @@
-package com.hdfc.employee.service;
+package com.hdfc.capstone.ems.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.hdfc.capstone.ems.exception.InvalidEmployeeIdException;
-import com.hdfc.capstone.ems.service.IEmployeeService;
 import com.hdfc.capstone.ems.vo.EmployeeVO;
 
 @SpringBootTest
 class EmployeeServiceImplTest {
 	
 	@Autowired
-	IEmployeeService employeeService;
+	IEmployeeService service;
 
 	@Test
 	void testGetEmployeeById() throws InvalidEmployeeIdException {
-		EmployeeVO employee = employeeService.getEmployeeById(1);
+		EmployeeVO employee = service.getEmployeeById(1);
 		assertEquals("Bharath Kumar", employee.getEmployeeName());
 	}
 
